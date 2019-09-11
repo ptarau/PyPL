09769f60882a9754fc2650b53f4724f2700aed2e
Paul Tarau <ptarau@gmail.com>
2019-09-11 08:51:53 -0500

    renamed subset1

--- subset.py
+++ subset.py
@@ -1,27 +1,27 @@
 import immutables as im
 
-def subset(Xs) : 
-  if not Xs : yield None
-  else :
-    X,Ys = Xs
-    for Zs in subset(Ys) :
-      yield Zs
-      yield (X,Zs)
-     
-def subset1(Xs) : 
+def subset(Xs) :
   if not Xs : yield Xs
   else :
     X = Xs[0]
     Ys = Xs[1:]
+    for Zs in subset(Ys) :
+      yield Zs
+      yield [X] + Zs
+
+def subset1(Xs) :
+  if not Xs : yield None
+  else :
+    X,Ys = Xs
     for Zs in subset1(Ys) :
       yield Zs
-      yield (X,) + Zs
-     
-t1 = list(subset((1,(2,(3,(4,None))))))   
+      yield (X,Zs)
+
+t1 = list(subset1((1,(2,(3,(4,None))))))
 
 def t2() : 
-  for X in subset(im.fromList([1,2,3,4])) : 
+  for X in subset1(im.fromList([1,2,3,4])) :
     print(im.toList(X))
     
 def t3() : 
-  for t in subset1( (0,1,2,3) ) : print(t)
+  for t in subset( [0,1,2,3] ) : print(t)
