d0f8af534df0451849eb51b47d4257c6ca15a46e
Paul Tarau <ptarau@gmail.com>
2019-09-13 08:37:23 -0500

    added s,k combinators and lambda examples

--- fun.py
+++ fun.py
@@ -60,6 +60,57 @@ def t1() :
   print(p)
   print(sum([1,2,34]))
 
+
+def cartprod(xs, ys):
+  return [(x, y) for x in xs for y in ys]
+
+
+lc1 = cartprod([1, 2], ['a', 'b', 'c'])
+# [(1, 'a'), (1, 'b'), (1, 'c'), (2, 'a'), (2, 'b'), (2, 'c')]
+
+lc2 = [(x, y) for x in [1, 2, 3] for y in [3, 1, 4] if x != y]
+
+
+def pythagoras(n):
+  for x in range(1, n):
+    for y in range(1, n):
+      for z in range(1, n):
+        if x * x + y * y == z * z:
+          yield (x, y, z)
+
+
+def pythagoras1(n):
+  return [(x, y, z) for x in range(1, n)
+          for y in range(1, n)
+          for z in range(1, n)
+          if x * x + y * y == z * z
+          ]
+
+
+def pythagoras2(u):
+  for m in range(1, u):
+    for n in range(1, m):
+      x = m ** 2 - n ** 2
+      y = 2 * m * n
+      z = m ** 2 + n ** 2
+      yield (x, y, z)
+
+
+'''
+>>> for p in pythagoras(20) : print(p)
+... 
+(3, 4, 5)
+(4, 3, 5)
+(5, 12, 13)
+(6, 8, 10)
+(8, 6, 10)
+(8, 15, 17)
+(9, 12, 15)
+(12, 5, 13)
+(12, 9, 15)
+(15, 8, 17)
+'''
+
 # examples of use
 
 print('hello')
