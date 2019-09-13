94057a0bdca7fdc5f339985bd5a2e9e09367942a
Paul Tarau <ptarau@gmail.com>
2019-09-12 22:12:13 -0500

    simpler permutation added

--- perm.py
+++ perm.py
@@ -1,39 +1,69 @@
+# insert an element in all possible places
+def ins(x,xs) :
+  for i  in range(len(xs)) :
+    yield xs[:i]+[x]+xs[i:]
+  yield xs + [x]
+
+# insert first in each permutation of the rest of a list
+def perm(xs) :
+  if not xs : yield []
+  else :
+    for ps in perm(xs[1:]) :
+      for rs in ins(xs[0],ps) :
+        yield rs
+
+
+# alternative, with immutable
+
 import immutables as im
 
-def selectFirst(Xs) :
-  if not Xs : return None
-  else :
-    X,Ys = Xs
-    yield (X,Ys)
-    for (Z,Zs) in selectFirst(Ys) :
-      yield (Z,(X,Zs))
-
-def perm(Xs) :
-  if not Xs : yield None
-  else :
-    for (X,Ys) in selectFirst(Xs) :
-      for Zs in perm(Ys) :
-        yield (X,Zs)
+def selectFirst(Xs):
+  if not Xs:
+    return None
+  else:
+    X, Ys = Xs
+    yield (X, Ys)
+    for (Z, Zs) in selectFirst(Ys):
+      yield (Z, (X, Zs))
 
 
-def permute(Ls) :
-  Is = im.fromList(Ls)  
-  for Ps in perm(Is) :
+def perm1(Xs):
+  if not Xs:
+    yield None
+  else:
+    for (X, Ys) in selectFirst(Xs):
+      for Zs in perm1(Ys):
+        yield (X, Zs)
+
+
+def permute(Ls):
+  Is = im.fromList(Ls)
+  for Ps in perm1(Is):
     yield im.toList(Ps)
-  
-  
-t1 = list(selectFirst((0,(1,(2,(3,None))))))
 
-t2 = list(perm((0,(1,(2,(3,(4,None)))))))
 
-t3 = im.fromList([0,1,2,3,4])
+# tests
 
-t4 = im.toList(t3)
+t1 = list(ins('*',[1,2,3]))
+
+def t2() :
+  for p in perm([1,2,3]) :
+    print(p)
+
+t5 = list(selectFirst((0,(1,(2,(3,None))))))
+
+t6 = list(perm1((0,(1,(2,(3,(4,None)))))))
+
+t7 = im.fromList([0,1,2,3,4])
+
+t8 = im.toList(t7)
 
 t5 = list(permute([1,2,3,4]))
 
-def go() :
+def t6() :
  for P in  permute([1,2,3,4]) :
    print(P)
+
+t2()
    
 
