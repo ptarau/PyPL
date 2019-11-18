80822db868b6f81a10aedb2078230dd364b16364
Paul Tarau <ptarau@gmail.com>
2019-11-18 10:40:10 -0600

    about threads in Java

---
+++ threads/src/threads/Main.java
@@ -0,0 +1,10 @@
+package threads;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("starting");
+        CubbyHole CH=new CubbyHole();
+    }
+}
