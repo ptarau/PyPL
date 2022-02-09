108a7c613a4f374edb7992cd79dc6da439bd8694
Paul Tarau <ptarau@gmail.com>
2022-02-08 20:20:54 -0600

    more itertools

--- generators.py
+++ generators.py
@@ -46,3 +46,86 @@ def unzip(xs):
 
 zs = list(zip(s1, s2))
 ls = list(zip(*zs))
+
+s3 = [0, 1, 2, 3, 40, 5, 6, 7, 8, 9, 10, 11]
+
+
+def small(x):
+    return x < 8
+
+
+t8 = list(takewhile(small, s3))
+print('t8', t8)
+
+t9 = list(dropwhile(small, s3))
+print('t9', t9)
+
+t8a = list(takewhile(lambda x: x < 8, s3))
+print('t8a', t8a)
+
+t9a = list(dropwhile(lambda x: x < 8, s3))
+print('t9a', t9a)
+
+i0 = cycle("abc")
+
+i1 = from_file('generators.py')
+
+i2 = tee(cycle("abc"))
+
+
+def sq(n):
+    """
+      returns square roots of
+      first n numbers
+    """
+    for x in range(n):
+        yield x ** 2
+
+
+def min_max(i):
+    i1, i2, i3 = tee(i, 3)
+    return min(i1), max(i2), sum(i3)
+
+
+def counts_from():
+    i = from_file('generators.py')
+    i1, i2 = tee(i)
+    return len(list(i1)), list(map(len, i2))
+
+
+print(counts_from())
+
+
+def drop(k, xs):
+    for _ in range(k):
+        next(xs)
+    return list(xs)
+
+
+t11 = nth(from_file('generators.py'), 5)
+print('t11', t11)
+
+t12 = list(islice(from_file('generators.py'), 103, 108))
+print(t12)
+
+"""
+hello
+"""
+
+
+def cprod(xs,ys):
+    for x in xs:
+        for y in ys:
+            yield (x,y)
+
+
+print(list(cprod("abc",[1,2,3,4])))
+
+print(list(product("ab",[1,2,3],"XY")))
+
+
+def cpower(xs,n):
+    yield from product(iter(xs),repeat=n)
+
+
+print(list(cpower([0,1],3)))
